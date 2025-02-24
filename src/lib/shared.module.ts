e3a2bc76f97ad8c85403181c827e2fe0caedb6b4
Nishaly-Elite <nishaly@elitecorpusa.com>
2025-02-24 19:51:27 +0530

    workflow commit

--- src/lib/shared.module.ts
+++ src/lib/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SharedComponent } from './shared.component';
 import { ProfileComponent } from './profile/profile.component';
+import { WorkflowComponent } from './workflow/workflow.component';
 
 
 @NgModule({
   declarations: [
-    SharedComponent, ProfileComponent
+    SharedComponent, ProfileComponent, WorkflowComponent
   ],
   imports: [
   ],
